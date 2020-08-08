3a981efaec565eb066848069aa3b8c6422ebf4f6
pguduguntla <praneethg347@gmail.com>
2020-08-08 12:04:07 -0700

    join class modal

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -23,7 +23,8 @@ class Sidebar extends Component {
     state = {
         user: null,
         doc: null,
-        createClassModal: false
+        createClassModal: false,
+        addClassModal: false
 
     };
 
@@ -62,12 +63,11 @@ class Sidebar extends Component {
     }
 
     createClass = () => {
-        console.log("hello");
         this.setState({ createClassModal: true })
     }
 
     addClass = () => {
-
+        this.setState({ addClassModal: true })
     }
 
     renderCreateClassModal() {
@@ -104,6 +104,49 @@ class Sidebar extends Component {
                         </span>
                         <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
                             <button type="button" onClick={() => this.setState({ createClassModal: false })} class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                Cancel
+                            </button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    renderAddClassModal() {
+        return (
+            <div class="z-40 fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center">
+                <div class="fixed inset-0 transition-opacity">
+                    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
+                <div class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div class="sm:flex sm:items-start">
+
+                            <div class="mt-3 text-center sm:mt-0 sm:ml-4 mr-4 sm:text-left sm:w-full">
+                                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                                    Join a class
+                                        </h3>
+                                <div class="mt-2">
+                                    <p class="text-sm leading-5 text-gray-500">
+                                        Enter your class code to join your classmates!
+                                        </p>
+                                </div>
+                                <div class="mt-2">
+                                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Enter class code..." />
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+                            <button type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-p-purple text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                                Join Class
+                            </button>
+                        </span>
+                        <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+                            <button type="button" onClick={() => this.setState({ addClassModal: false })} class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                                 Cancel
                             </button>
                         </span>
@@ -258,6 +301,7 @@ class Sidebar extends Component {
                 {this.renderClassesSidebar()}
                 {this.renderMainSidebar()}
                 {this.state.createClassModal ? this.renderCreateClassModal() : <div></div>}
+                {this.state.addClassModal ? this.renderAddClassModal() : <div></div>}
             </div>
         );
     }
