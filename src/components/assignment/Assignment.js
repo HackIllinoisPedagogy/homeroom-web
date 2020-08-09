d5a425af965aeb5bc65fad4e465d129f0ae8b353
Ashish Rao <ashish.arartc@gmail.com>
2020-08-09 12:28:40 -0700

    rendering changes

--- src/components/assignment/Assignment.js
+++ src/components/assignment/Assignment.js
@@ -77,47 +77,6 @@ class Assignment extends React.Component {
 
 	}
 
-	handleLatexRendering(fragment) {
-		if (fragment.includes("$")) {
-			return <InlineMath>{fragment.substring(1, fragment.length-1)}</InlineMath>;
-		}
-		return fragment;
-	}
-
-	generateRenderingArray(str) {
-		let out = [];
-		let latex = str.match(/[$].*?[$]/g);
-		let text = str.split(/[$].*?[$]/g);
-
-		if (latex == null) {
-			return text;
-		}
-
-		if (str[0] === "$") {
-			let temp = text;
-			text = latex;
-			latex = temp;
-		}
-
-		let count = 0;
-		while (true) {
-			if (count % 2 === 0) {
-				if (count/2 > text.length - 1) {
-					break;
-				}
-				out.push(text[count/2]);
-			} else {
-				if ((count-1)/2 > latex.length - 1) {
-					break;
-				}
-				out.push(latex[(count-1)/2])
-			}
-			count++;
-		}
-
-		return out;
-	}
-
 	changeProblem(i) {
 		this.setState({
 			//problems: this.state.problems,
